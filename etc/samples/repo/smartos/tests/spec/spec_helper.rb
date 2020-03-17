cc187d4922449ab0c7467a6c15602471192ebc30
Jack <nonepassby@163.com>
2020-03-17 04:13:29 +0000

    add repo temp samples

---
+++ etc/samples/repo/smartos/tests/spec/spec_helper.rb
@@ -0,0 +1,20 @@
+require 'serverspec'
+require 'pathname'
+require 'net/ssh'
+require 'yaml'
+
+include Serverspec::Helper::Ssh
+include Serverspec::Helper::DetectOS
+include Serverspec::Helper::Properties
+
+properties = YAML.load_file('properties.yml')
+
+RSpec.configure do |c|
+  c.host  = ENV['TARGET_HOST']
+  set_property properties[c.host]
+  options = Net::SSH::Config.for(c.host)
+  user    = options[:user] || Etc.getlogin
+  c.ssh   = Net::SSH.start(c.host, user, options)
+  c.os    = backend.check_os
+  #c.path  = '/usr/local/sbin:/usr/local/bin:/opt/local/sbin:/opt/local/bin:/usr/sbin:/usr/bin:/sbin'
+end
